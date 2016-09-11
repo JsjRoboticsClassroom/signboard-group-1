695e1431e49ffc672e54d719cb3d7bfae4dfc147
Rafael Soto <rooksoto@gmail.com>
2016-09-10 21:26:13 -0400

    Fourth Commit

--- src/nyc/c4q/ac21/signboard/Main.java
+++ src/nyc/c4q/ac21/signboard/Main.java
@@ -75,7 +75,7 @@ public class Main {
 
 
 
-    public static void scrollTextScene(SignBoard board, String text[]) {
+    public static void scrollTextSceneCustom1(SignBoard board, String text[]) {
         int width = board.getWidth();
         int y = board.getHeight() / 2;
 
@@ -139,6 +139,65 @@ public class Main {
         }
     }
 
+    public static void scrollTextSceneCustom2(SignBoard board, String text1, String text2) {
+        Random random = new Random();
+        String text = text1;
+        int frameSwitchRate = 10;
+        int frameCount = 0;
+        int width = board.getWidth();
+        int y = board.getHeight() / 2;
+        for (int x = -text.length(); x <= width; ++x) {
+
+            SignBoard.Frame frame = board.newFrame();
+
+            frameCount++;
+
+            if (frameCount % frameSwitchRate == 0) {
+
+                if (text.equalsIgnoreCase(text1)) {
+                    text = text2;
+                } else if (text.equalsIgnoreCase(text2)) {
+                    text = text1;
+                }
+            }
+
+            int color = random.nextInt(4);
+
+            if (color == 0) {
+                for (int i = 0; i < 10; i++) {
+                    frame.setGreen();
+                }
+            } else if (color == 1) {
+                for (int i = 0; i < 10; i++) {
+                    frame.setRed();
+                }
+            } else if (color == 2) {
+                for (int i = 0; i < 10; i++) {
+                    frame.setWhite();
+                }
+            } else {
+                for (int i = 0; i < 10; i++) {
+                    frame.setYellow();
+                }
+            }
+
+            if (x >= width)
+                break;
+
+            if (x < 0)
+                // Scrolling on to the left side.
+                frame.write(0, y, text.substring(-x));
+            else if (x + text.length() <= width) {
+                // Fully on the board.
+                frame.write(x, y, text);
+            } else
+                // Scrolling off the board.
+                frame.write(x, y, text.substring(0, width - x));
+            frame.finish(0.05);
+        }
+    }
+
+
     /**
      * Draws a scene which flashes the words "FRESH" and "HOT".
      * @param board
@@ -146,7 +205,7 @@ public class Main {
      * @param cycles
      *   The number of cycles to draw for.
      */
-    public static void flashFreshHotScene(SignBoard board, int cycles) {
+    public static void flashFreshHotSceneCustom(SignBoard board, int cycles) {
         Random random = new Random();
         int width = board.getWidth();
         int leftPosition = width / 4 - 12;
@@ -193,17 +252,69 @@ public class Main {
         }
     }
 
+    public static void ribbonSceneCustom(SignBoard board, int numCycles, String ribbonText1, String ribbonText2) {
+
+        int width = board.getWidth();
+        int height = board.getHeight();
+
+        Random random = new Random(width);
+
+        for (int i = 0; i < numCycles; ++i) {
+            SignBoard.Frame frame = board.newFrame();
+
+            int color1 = random.nextInt(4);
+            switch (color1) {
+                case 0:
+                    frame.setGreen();
+                    break;
+                case 1:
+                    frame.setRed();
+                    break;
+                case 2:
+                    frame.setWhite();
+                    break;
+                case 3:
+                    frame.setYellow();
+                    break;
+            }
+            int x = random.nextInt(width);
+            if ((x + ribbonText1.length()) > width) {
+                x -= (ribbonText1.length());
+            }
+
+            int y = random.nextInt(height);
+
+            frame.write(x, y, ribbonText1);
+
+            x = random.nextInt(width);
+            if ((x + ribbonText2.length()) > width) {
+                x -= (ribbonText2.length());
+            }
+            y = random.nextInt(height);
+
+            frame.write(x, y, ribbonText2);
+
+
+            frame.finish(0.25);
+        }
+    }
+
+
+
     public static void main(String[] args) {
         SignBoard signBoard = new SignBoard(8);
 
         // Run the sign board forever.
         while (true) {
-            scrollTextScene(signBoard, dollarSign1);
-            scrollTextScene(signBoard, dollarSign2);
-            scrollTextScene(signBoard, dollarSign3);
-            scrollTextScene(signBoard, dollar);
 
-            flashFreshHotScene(signBoard, 8);
+
+            scrollTextSceneCustom2(signBoard, "$$$$$ C A S I N O $$$$$", "$$$$$    C.4.Q    $$$$$");
+            scrollTextSceneCustom1(signBoard, dollarSign1);
+            scrollTextSceneCustom1(signBoard, dollarSign2);
+            scrollTextSceneCustom1(signBoard, dollarSign3);
+            ribbonSceneCustom(signBoard, 16, "Casino Night", "Win Big!!!");
+            flashFreshHotSceneCustom(signBoard, 8);
+            scrollTextSceneCustom2(signBoard, "$$$$$ C A S I N O $$$$$", "$$$$$    C.4.Q    $$$$$");
         }
     }
 }
