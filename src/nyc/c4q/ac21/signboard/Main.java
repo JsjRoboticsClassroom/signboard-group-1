2a55b44cdbaaa86e90d2097460e56af5206086e2
Rafael Soto <rooksoto@gmail.com>
2016-09-10 21:07:21 -0400

    Third Commit

--- src/nyc/c4q/ac21/signboard/Main.java
+++ src/nyc/c4q/ac21/signboard/Main.java
@@ -109,6 +109,196 @@ public class Main {
         }
     }
 
+
+
+    public static String dollarSign3[]={
+
+
+
+            "SSSSSSSSSs. SSSSSSSSSs. SSSSSSSSSs.\n",
+            "SSSSSSSSSSS SSSSSSSSSSS SSSSSSSSSSS\n",
+            "     S SSS       S SSS       S SSS \n",
+            "   S  SS       S  SS       S  SS   \n",
+            "  S..SS       S..SS       S..SS    \n",
+            " S:::S       S:::S       S:::S     \n",
+            "S;;;S       S;;;S       S;;;S      \n",
+            "S%%%S       S%%%S       S%%%S      \n"
+
+
+
+
+    };
+
+
+    public static String dollarSign2[]={
+
+
+            "SSSSSSSSSs. SSSSSSSSSs. \n",
+            "SSSSSSSSSSS SSSSSSSSSSS \n",
+            "     S SSS       S SSS  \n",
+            "   S  SS       S  SS    \n",
+            "  S..SS       S..SS     \n",
+            " S:::S       S:::S      \n",
+            "S;;;S       S;;;S       \n",
+            "S%%%S       S%%%S       \n"
+
+
+
+
+
+
+    };
+
+    public static String dollar[]={
+            "    A           A     \n",
+            " .d8888.     .d8888.  \n",
+            " 88'8 YP     88'8 YP  \n",
+            " `8b8.       `8b8.    \n",
+            "   `V8b.       `V8b.  \n",
+            " db 8 8D     db 8 8D  \n",
+            " `8888Y'     `8888Y'  \n",
+            "    V           V     \n"
+
+
+
+    };
+
+
+    public static String dollarSign1[]={
+
+
+            "SSSSSSSSSs.\n",
+            "SSSSSSSSSSS\n",
+            "     S SSS \n",
+            "   S  SS   \n",
+            "  S..SS    \n",
+            " S:::S     \n",
+            "S;;;S      \n",
+            "S%%%S      \n"
+
+
+    };
+
+
+
+    public static void scrollTextSceneCustom(SignBoard board, String text[]) {
+        int width = board.getWidth();
+        int y = board.getHeight() / 2;
+
+        for (int x = -text[0].length()+6; x <= width; ++x) {
+            SignBoard.Frame frame = board.newFrame();
+
+            if (x >= width)
+                break;
+
+            if (x < 0){
+                boolean colorFlag = false;
+                int delta=-text.length/2;
+                // Scrolling on to the left side.
+                for(int j=0;j<text.length;j++) {
+                    frame.setRed();
+                    if(colorFlag){
+                        frame.setGreen();
+                    }
+
+                    frame.write(0, y+delta, text[j].substring(-x));
+                    colorFlag=!colorFlag;
+                    delta ++;
+                }
+
+
+            }
+
+            else if (x + text[0].length()<= width){
+                boolean colorFlag = false;
+                int delta=-text.length/2;
+                // Fully on the board.
+                for(int j=0;j<text.length;j++) {
+                    frame.setRed();
+                    if(colorFlag){
+                        frame.setWhite();
+                    }
+                    frame.write(x, y+delta, text[j]);
+                    delta ++;
+                    colorFlag=!colorFlag;
+                }
+
+            }
+            else{
+                // Scrolling off the board.
+                boolean colorFlag = false;
+                int delta=-text.length/2;
+                for(int j=0;j<text.length;j++) {
+                    frame.setGreen();
+                    if(colorFlag){
+                        frame.setWhite();
+                    }
+                    frame.write(x, y+delta, text[j].substring(0, width - x));
+                    colorFlag=!colorFlag;
+                    delta ++;
+
+                }
+
+            }
+
+            frame.finish(0.02);
+        }
+    }
+
+    /**
+     * Draws a scene which flashes the words "FRESH" and "HOT".
+     * @param board
+     *   The board on which to draw.
+     * @param cycles
+     *   The number of cycles to draw for.
+     */
+    public static void flashFreshHotSceneCustom(SignBoard board, int cycles) {
+        Random random = new Random();
+        int width = board.getWidth();
+        int leftPosition = width / 4 - 12;
+        int rightPosition = 2 * width / 4 - 7;
+        int y = board.getHeight() / 2;
+        boolean colorFlag= false;
+        for (int i = 0; i < cycles * 2; ++i) {
+            SignBoard.Frame frame = board.newFrame();
+
+
+
+            // Write a word.
+            if (i % 2 == 0) {
+                frame.setRed();
+                if(colorFlag){frame.setWhite();}
+                frame.write(leftPosition, y - 4, "    /$$$$$                     /$$     ");
+                frame.write(leftPosition, y - 3, "   |__  $$                    | $$     ");
+                frame.write(leftPosition, y - 2, "      | $$  /$$$$$$   /$$$$$$$| $$   /$$");
+                frame.write(leftPosition, y - 1, "      | $$ |____  $$ /$$_____/| $$  /$$/");
+                frame.write(leftPosition, y    , " /$$  | $$  /$$$$$$$| $$      | $$$$$$/ ");
+                frame.write(leftPosition, y + 1, "| $$  | $$ /$$__  $$| $$      | $$_  $$ ");
+                frame.write(leftPosition, y + 2, "|  $$$$$$/|  $$$$$$$|  $$$$$$$| $$ \\  $$");
+                frame.write(leftPosition, y + 3, "\\______/ \\_______/\\_______/|__/  \\__/");
+
+                colorFlag=!colorFlag;
+            }
+            else {
+
+                frame.setGreen();
+
+
+                frame.write(rightPosition, y - 4, " /$$$$$$$             /$$");
+                frame.write(rightPosition, y - 3, "| $$__  $$           | $$");
+                frame.write(rightPosition, y - 2, "| $$ \\ $$ /$$$$$$  /$$$$$$");
+                frame.write(rightPosition, y - 1, "| $$$$$$$//$$__  $$|_  $$_/");
+                frame.write(rightPosition, y    , "| $$____/| $$ \\ $$  | $$ ");
+                frame.write(rightPosition, y + 1, "| $$     | $$  | $$  | $$ /$$");
+                frame.write(rightPosition, y + 2, "| $$     |  $$$$$$/  |  $$$$/");
+                frame.write(rightPosition, y + 3, "|__/     \\______/   \\___/ ");
+
+            }
+
+            frame.finish(0.25);
+        }
+    }
+
     public static void main(String[] args) {
         SignBoard signBoard = new SignBoard(8);
 
@@ -116,7 +306,13 @@ public class Main {
         while (true) {
 
             scrollTextSceneCustom(signBoard, "$$$$$ C A S I N O $$$$$", "$$$$$    C.4.Q    $$$$$");
+            scrollTextSceneCustom(signBoard, dollarSign1);
+            scrollTextSceneCustom(signBoard, dollarSign2);
+            scrollTextSceneCustom(signBoard, dollarSign3);
             ribbonSceneCustom(signBoard, 16, "Casino Night", "Win Big!!!");
+            flashFreshHotSceneCustom(signBoard, 8);
+            scrollTextSceneCustom(signBoard, "$$$$$ C A S I N O $$$$$", "$$$$$    C.4.Q    $$$$$");
+
         }
     }
 }
